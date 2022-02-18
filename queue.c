787ce90781c8e0e58734c9d3c3a39ddcf01bdddc
Kevin Shih <kevin89428@gmail.com>
2022-02-19 01:51:31 +0800

    Implement q_reverse
    Implement with list_for_each_safe() to ensure traverse list correctly
    while modifying `next` and `prev` pointers.

--- queue.c
+++ queue.c
@@ -181,7 +181,18 @@ void q_swap(struct list_head *head)
  * (e.g., by calling q_insert_head, q_insert_tail, or q_remove_head).
  * It should rearrange the existing ones.
  */
-void q_reverse(struct list_head *head) {}
+void q_reverse(struct list_head *head)
+{
+    struct list_head *node, *safe;
+
+    list_for_each_safe (node, safe, head) {
+        node->next = node->prev;
+        node->prev = safe;
+    }
+    safe = head->prev;
+    head->prev = head->next;
+    head->next = safe;
+}
 
 /*
  * Sort elements of queue in ascending order
